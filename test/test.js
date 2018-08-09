838d9535534f09a0234c78ff1c6fa9f42d87443d
gdufay <gdufay@student.42.fr>
2018-08-09 14:43:55 +0000

    finish installing test

--- test/test.js
+++ test/test.js
@@ -1,8 +1,44 @@
-const assert = require('assert');
-describe('Array', function() {
-  describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal([1,2,3].indexOf(4), 0);
-    });
-  });
+const request = require('request');
+const expect = require('chai').expect;
+const sequelize = require('sequelize');
+const models = require("../models");
+
+/******* REGISTER *******/
+
+describe('routes/Users.js', function() {
+	before((done) => {
+		models.User.destroy({
+			where: {}
+		})
+			.then(() => {
+				done();
+			});
+	});
+
+
+	describe('#register()', function() {
+		const url = "http://localhost:8080/api/users/register/";
+		const infos = {
+			email: 'abcd@abcd.com',
+			username: 'testa',
+			name: 'test',
+			password: 'abcABC1',
+			img: null,
+			first_name: 'test'
+		};
+
+		it('should return 201', function(done) {
+			request.post(url, {form: infos}, function (err, res, body) {
+				expect(res.statusCode).to.equal(201);
+				done();
+			});
+		});
+
+		it('should return 409', function(done) {
+			request.post(url, {form: infos}, function (err, res, body) {
+				expect(res.statusCode).to.equal(409);
+				done();
+			});
+		});
+	});
 });
