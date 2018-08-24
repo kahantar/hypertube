3ebf0d541763ebb270828cc5cb4b22425ba16f38
gdufay <gdufay@student.42.fr>
2018-08-24 10:06:43 +0000

    add .gitignore

---
+++ config/keysAuth.js
@@ -0,0 +1,9 @@
+const id = {
+    google: {
+        clientID: '1044181093375-7opl6a3rlkgj8deqmdq7fr4990eub6au.apps.googleusercontent.com',
+        clientSecret: 'wHJ1PmFvcIxLmP0u_mM3ZkjL',
+        callbackURL: 'http://localhost:8080/auth/google/callback'
+    }
+};
+
+module.exports = id;
