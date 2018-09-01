8e989fd7bb372e45c492049b28c19c6711b918c0
AHANTAR <ahantar@MacBook-Air-de-AHANTAR.local>
2018-09-02 00:40:46 +0200

    Ebauche design page register

--- frontend/src/components/register/form.js
+++ frontend/src/components/register/form.js
@@ -3,6 +3,8 @@ import './style.css';
 import { registerUser } from '../../actions/user'
 import { connect } from 'react-redux'
 import {bindActionCreators} from 'redux'
+import GoogleButton from 'react-google-button'
+
 
 class Form extends React.Component{
     state = {
@@ -20,14 +22,17 @@ class Form extends React.Component{
     }
     render(){
         return (
-            <form id="conteneur" onSubmit={(e) => this.handleSubmit(e)}>
-                <input type="email "value={this.state.email} placeholder="Email" onChange={(event) => this.setState({email: event.target.value})}/>
-                <input type="text"value={this.state.username} placeholder="Username" onChange={(event) => this.setState({username: event.target.value})}/>
-                <input type="text" value={this.state.name} placeholder="Name" onChange={(event) => this.setState({name: event.target.value})}/>
-                <input type="text" value={this.state.first_name} placeholder="First_name" onChange={(event) => this.setState({first_name: event.target.value})}/>
-                <input type="password" value={this.state.password} placeholder="Password" onChange={(event) => this.setState({password: event.target.value})}/>
-                <input type="password" value={this.state.confirm_password} placeholder="Confirmation Password" onChange={(event) => this.setState({confirm_password: event.target.value})}/>                
-                <button type="submit">S'inscrire</button>
+            <form id="form" Class="card row justify-content-md-around" onSubmit={(e) => this.handleSubmit(e)}>
+                <img src="https://fontmeme.com/permalink/180901/3bd2426f867386d0ba5efba6386554cd.png" />                
+                <h2 id="titre">S'inscrire</h2>
+                <input id="input" Class="input-lg form-control" type="email "value={this.state.email} placeholder="E-mail" onChange={(event) => this.setState({email: event.target.value})}/>
+                <input id="input" Class="input-lg form-control" type="text"value={this.state.username} placeholder="Username" onChange={(event) => this.setState({username: event.target.value})}/>
+                <input id="input" Class="input-lg form-control" type="text" value={this.state.name} placeholder="Name" onChange={(event) => this.setState({name: event.target.value})}/>
+                <input id="input" Class="input-lg form-control" type="text" value={this.state.first_name} placeholder="First_name" onChange={(event) => this.setState({first_name: event.target.value})}/>
+                <input id="input" Class="input-lg form-control" type="password" value={this.state.password} placeholder="Password" onChange={(event) => this.setState({password: event.target.value})}/>
+                <input id="input" Class="input-lg form-control" type="password" value={this.state.confirm_password} placeholder="Confirmation Password" onChange={(event) => this.setState({confirm_password: event.target.value})}/>                
+                <button id="button" Class="btn btn-lg btn-danger" type="submit">S'inscrire</button>
+                <GoogleButton />
             </form>
         );
     }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,6 +5,7 @@ import Routes from './routes';
 import { BrowserRouter } from 'react-router-dom';
 import createAppStore from './store';
 import { PersistGate } from 'redux-persist/es/integration/react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const { persistor, store } = createAppStore()
 
