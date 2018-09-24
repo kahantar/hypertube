a5c06a099d51ce6e098be6493f9ebf8ec6a42f06
Ugo Nicolai <unicolai@student.42.fr>
2018-09-24 22:24:56 +0200

    add picture cloudinary

--- frontend/src/components/register/form.js
+++ frontend/src/components/register/form.js
@@ -23,32 +23,32 @@ class Form extends React.Component{
         return (
             <form id="form" onSubmit={(e) => this.handleSubmit(e)}>
                 <div className='Register_frame'>
-                    <img className='logoForm' src={MailImg} alt='mail'/>                
+                    <img className='logoForm' src='https://res.cloudinary.com/dzhnhtkyv/image/upload/v1537541388/Netflix42/mail_dgwbct.png' alt='mail'/>                
                     <input id="email" type="email "value={this.state.email} placeholder="Mail" onChange={(event) => this.setState({email: event.target.value})}/>
                 </div>
                 <div className='Register_line'/>
                 <div className='Register_frame'>
-                    <img className='logoForm' src={MailImg} alt='mail'/>
+                    <img className='logoForm' src='https://res.cloudinary.com/dzhnhtkyv/image/upload/v1537545396/Netflix42/user.png' alt='user'/>
                     <input id="username" type="text" value={this.state.username} placeholder="Username" onChange={(event) => this.setState({username: event.target.value})}/>
                 </div>
                 <div className='Register_line'/>
                 <div className='Register_frame'>
-                    <img className='logoForm' src={MailImg} alt='mail'/>
+                    <img className='logoForm' src='https://res.cloudinary.com/dzhnhtkyv/image/upload/v1537545396/Netflix42/user.png' alt='user'/>
                     <input id="first" type="text" value={this.state.first_name} placeholder="First Name" onChange={(event) => this.setState({first_name: event.target.value})}/>
                 </div>
                 <div className='Register_line'/>
                 <div className='Register_frame'>
-                    <img className='logoForm' src={MailImg} alt='mail'/>
+                    <img className='logoForm' src='https://res.cloudinary.com/dzhnhtkyv/image/upload/v1537545396/Netflix42/user.png' alt='user'/>
                     <input id="name" type="text" value={this.state.name} placeholder="Second Name" onChange={(event) => this.setState({name: event.target.value})}/>                
                 </div>
                 <div className='Register_line'/>
                 <div className='Register_frame'>
-                    <img className='logoForm' src={PwdImg} alt='password'/>
+                    <img className='logoForm' src='https://res.cloudinary.com/dzhnhtkyv/image/upload/v1537543400/Netflix42/pwd.png' alt='password'/>
                     <input id="password" type="password" value={this.state.password} placeholder="Password" onChange={(event) => this.setState({password: event.target.value})}/>
                 </div>
                 <div className='Register_line'/>
                 <div className='Register_frame'>
-                    <img className='logoForm' src={PwdImg} alt='password'/>
+                    <img className='logoForm' src='https://res.cloudinary.com/dzhnhtkyv/image/upload/v1537543400/Netflix42/pwd.png' alt='password'/>
                     <input id="confirm" type="password" value={this.state.confirm_password} placeholder="Confirmation" onChange={(event) => this.setState({confirm_password: event.target.value})}/>                
                 </div>
                 <div className='Register_line'/>
