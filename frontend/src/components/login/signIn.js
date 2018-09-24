3ed1e061fdc0d87db640cb3a55c40a9fd16d2a8c
Ugo Nicolai <unicolai@student.42.fr>
2018-09-24 23:10:42 +0200

    fix link button without bootstrap

--- frontend/src/components/login/signIn.js
+++ frontend/src/components/login/signIn.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const SignIn = (props) => {
     return (
-        <button className='Login_buttonApi' href={props.link}>
+        <a className='Login_buttonApi' href={props.link}>
             <img className='Login_imgApi' src={props.img} alt="signInWithApi"/>
             <div className='Login_textApi'>{props.text}</div>
-        </button>
+        </a>
     )
 }
 
