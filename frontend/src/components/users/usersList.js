03bfa51203dcba71ca191efab7e60382c89de243
Ugo Nicolai <unicolai@student.42.fr>
2018-09-30 00:57:10 +0200

    start carousel all users

--- frontend/src/components/users/usersList.js
+++ frontend/src/components/users/usersList.js
@@ -12,7 +12,7 @@ class UsersList extends React.Component{
         console.log(users)
         return(
             <div id="UserList_carousel">
-                <div >{(users[this.state.i - 2]) ? users[this.state.i - 2] : null}</div>
+                <div id='UserList_prev2'>{(users[this.state.i - 2]) ? users[this.state.i - 2] : null}</div>
                 <div id='UserList_prev1'>{(users[this.state.i - 1]) ? users[this.state.i - 1] : null}</div>
                 <div id='UserList_selected'>{(users[this.state.i]) ? users[this.state.i] : null}</div>
                 <div id='UserList_next1'>{(users[this.state.i + 1]) ? users[this.state.i + 1] : null}</div>
