4ce34a15387cee9484d129cba788f4c5600a78a9
Ugo Nicolai <unicolai@student.42.fr>
2018-09-30 23:54:34 +0200

    carousel works

--- frontend/src/components/users/usersList.js
+++ frontend/src/components/users/usersList.js
@@ -1,90 +1,263 @@
 import React from 'react';
-import DisplayUser from './displayUser';
+// import DisplayUser from './displayUser';
 import './users.css';
 
 
 class UsersList extends React.Component{
-    movePrev2 = () => {
+    // movePrev2 = () => {
+    //     this.setState({
+    //         hideLeft2Style: this.state.prev2Style,
+    //         hideLeft2Click: this.movePrev2,
+    //         hideLeft1Style: this.state.prev1Style,
+    //         hideLeft1Click: this.movePrev1,
+    //         prev2Style: this.state.selectedStyle,
+    //         prev2Click: this.moveSelected,
+    //         prev1Style: this.state.next1Style,
+    //         prev1Click: this.moveNext1,
+    //         selectedStyle: this.state.next2Style,
+    //         selectedClick: this.moveNext2,
+    //         next1Style: this.state.hideRight1Style,
+    //         next1Click: null,
+    //         next2Style: this.state.hideRight2Style,
+    //         next2Click: null,
+    //         hideRight1Style: this.state.hideLeft1Style,
+    //         hideRight1Click: null,
+    //         hideRight2Style: this.state.hideLeft2Style,
+    //         hideRight2Click: null
+    //     })
+    // }
+
+    // movePrev1 = () => {
+    //     this.setState({
+    //         hideLeft2Style: this.state.hideLeft1Style,
+    //         hideLeft2Click: null,
+    //         hideLeft1Style: this.state.prev2Style,
+    //         hideLeft1Click: this.movePrev2,
+    //         prev2Style: this.state.prev1Style,
+    //         prev2Click: this.movePrev1,
+    //         prev1Style: this.state.selectedStyle,
+    //         prev1Click: this.moveSelected,
+    //         selectedStyle: this.state.next1Style,
+    //         selectedClick: this.moveNext1,
+    //         next1Style: this.state.next2Style,
+    //         next1Click: this.moveNext2,
+    //         next2Style: this.state.hideRight1Style,
+    //         next2Click: null,
+    //         hideRight1Style: this.state.hideRight2Style,
+    //         hideRight1Click: null,
+    //         hideRight2Style: this.state.hideLeft2Style,
+    //         hideRight2Click: null
+    //     })
+    // }
+
+    // moveSelected = () => {
+    //     console.log('s')
+    // }
+
+    // moveNext1 = () => {
+    //     console.log('n1')
+    // }
+
+    // moveNext2 = () => {
+    //     console.log('n2')
+    // }
+
+    // state = {
+    //     i: 2,
+    //     hideLeft2Style: {transition: 'transform 1s ease-out, width 1s ease-out, height 1s ease-out, left 1s ease-out, opacity 1s ease-out', left: '-30%', zIndex: '0', height: '17vw', maxHeight: '30vh', width: '10vw', maxWidth: '32vh', top: '50%', transform: 'translate(0, -50%)', opacity: '0'},
+    //     hideLeft2Click: null,
+    //     hideLeft1Style: {transition: 'transform 1s ease-out, width 1s ease-out, height 1s ease-out, left 1s ease-out, opacity 1s ease-out', left: '-15%', zIndex: '1', height: '22vw', maxHeight: '40vh', width: '15vw', maxWidth: '37vh', top: '50%', transform: 'translate(0, -50%)', opacity: '0'},
+    //     hideLeft1Click: null,
+    //     prev2Style: {transition: 'transform 1s ease-out, width 1s ease-out, height 1s ease-out, left 1s ease-out, opacity 1s ease-out', left: '0%', zIndex: '2', height: '27vw', maxHeight: '50vh', width: '20vw', maxWidth: '42vh', top: '50%', transform: 'translate(0, -50%)'},
+    //     prev2Click: this.movePrev2,
+    //     prev1Style: {transition: 'transform 1s ease-out, width 1s ease-out, height 1s ease-out, left 1s ease-out, opacity 1s ease-out', left: '15%', zIndex: '3', height: '32vw', maxHeight: '60vh', width: '25vw', maxWidth: '47vh', top: '50%', transform: 'translate(0, -50%)'},
+    //     prev1Click: this.movePrev1,
+    //     selectedStyle: {transition: 'transform 1s ease-out, width 1s ease-out, height 1s ease-out, left 1s ease-out, opacity 1s ease-out', left: '50%', transform: 'translate(-50%, -50%)', zIndex: '4', top: '50%', height: '37vw', maxHeight: '70vh', width: '30vw', maxWidth: '57vh'},
+    //     selectedClick: this.moveSelected,
+    //     next1Style: {transition: 'transform 1s ease-out, width 1s ease-out, height 1s ease-out, left 1s ease-out, opacity 1s ease-out', left: '85%', zIndex: '3', height: '32vw', maxHeight: '60vh', width: '25vw', maxWidth: '47vh', top: '50%', transform: 'translate(-100%, -50%)'},
+    //     next1Click: this.moveNext1,
+    //     next2Style: {transition: 'transform 1s ease-out, width 1s ease-out, height 1s ease-out, left 1s ease-out, opacity 1s ease-out', left: '100%', zIndex: '2', height: '27vw', maxHeight: '50vh', width: '20vw', maxWidth: '42vh', top: '50%', transform: 'translate(-100%, -50%)'},
+    //     next2Click: this.moveNext2,
+    //     hideRight1Style: {transition: 'transform 1s ease-out, width 1s ease-out, height 1s ease-out, left 1s ease-out, opacity 1s ease-out', left: '98%', zIndex: '1', height: '22vw', maxHeight: '40vh', width: '15vw', maxWidth: '37vh', top: '50%', transform: 'translate(0, -50%)', opacity: '0'},
+    //     hideRight1Click: null,
+    //     hideRight2Style: {transition: 'transform 1s ease-out, width 1s ease-out, height 1s ease-out, left 1s ease-out, opacity 1s ease-out', left: '110%', zIndex: '0', height: '17vw', maxHeight: '30vh', width: '10vw', maxWidth: '32vh', top: '50%', transform: 'translate(0, -50%)', opacity: '0'},
+    //     hideRight2Click: null
+
+    state = {
+        hideLeft: 'hideLeft',
+        prevLeftSecond: 'prevLeftSecond',
+        prev: 'prev',
+        selected: 'selected',
+        next: 'next',
+        nextRightSecond: 'nextRightSecond',
+        hideRight: 'hideRight'
+    }
+
+    // moveToSelected = (e) => {
+    //     console.log(e)
+    //     // var selected = element;
+      
+    //     // var next = $(selected).next();
+    //     // var prev = $(selected).prev();
+    //     // var prevSecond = $(prev).prev();
+    //     // var nextSecond = $(next).next();
+      
+    //     // $(selected).removeClass().addClass("selected");
+      
+    //     // $(prev).removeClass().addClass("prev");
+    //     // $(next).removeClass().addClass("next");
+      
+    //     // $(nextSecond).removeClass().addClass("nextRightSecond");
+    //     // $(prevSecond).removeClass().addClass("prevLeftSecond");
+      
+    //     // $(nextSecond).nextAll().removeClass().addClass('hideRight');
+    //     // $(prevSecond).prevAll().removeClass().addClass('hideLeft');
+      
+    // }
+      
+      // Eventos teclado
+    //   $(document).keydown(function(e) {
+    //       switch(e.which) {
+    //           case 37: // left
+    //           moveToSelected('prev');
+    //           break;
+      
+    //           case 39: // right
+    //           moveToSelected('next');
+    //           break;
+      
+    //           default: return;
+    //       }
+    //       e.preventDefault();
+    //   });
+      
+    //   $('#carousel div').click(function() {
+    //     moveToSelected($(this));
+    //   });
+      
+    //   $('#prev').click(function() {
+    //     moveToSelected('prev');
+    //   });
+      
+    //   $('#next').click(function() {
+    //     moveToSelected('next');
+    //   });
+    // }
+
+    prev3MoveToSelected = () => {
         this.setState({
-            hideLeft2Style: this.state.prev2Style,
-            hideLeft2Click: this.movePrev2,
-            hideLeft1Style: this.state.prev1Style,
-            hideLeft1Click: this.movePrev1,
-            prev2Style: this.state.selectedStyle,
-            prev2Click: this.moveSelected,
-            prev1Style: this.state.next1Style,
-            prev1Click: this.moveNext1,
-            selectedStyle: this.state.next2Style,
-            selectedClick: this.moveNext2,
-            next1Style: this.state.hideRight1Style,
-            next1Click: null,
-            next2Style: this.state.hideRight2Style,
-            next2Click: null,
-            hideRight1Style: this.state.hideLeft1Style,
-            hideRight1Click: null,
-            hideRight2Style: this.state.hideLeft2Style,
-            hideRight2Click: null
+            hideLeft: 'selected',
+            prevLeftSecond: 'next',
+            prev: 'nextRightSecond',
+            selected: 'hideRight',
+            next: 'hideLeft',
+            nextRightSecond: 'prevLeftSecond',
+            hideRight: 'prev'
         })
     }
 
-    movePrev1 = () => {
-        console.log('prev1')
+    prev2MoveToSelected = () => {
+        this.setState({
+            hideLeft: 'prev',
+            prevLeftSecond: 'selected',
+            prev: 'next',
+            selected: 'nextRightSecond',
+            next: 'hideRight',
+            nextRightSecond: 'hideLeft',
+            hideRight: 'prevLeftSecond'
+        })
     }
 
-    moveSelected = () => {
-        console.log('s')
+    prev1MoveToSelected = () => {
+        this.setState({
+            hideLeft: 'prevLeftSecond',
+            prevLeftSecond: 'prev',
+            prev: 'selected',
+            selected: 'next',
+            next: 'nextRightSecond',
+            nextRightSecond: 'hideRight',
+            hideRight: 'hideLeft'
+        })
     }
 
-    moveNext1 = () => {
-        console.log('n1')
+    moveToSelected = () => {
+        this.setState({
+            hideLeft: 'hideLeft',
+            prevLeftSecond: 'prevLeftSecond',
+            prev: 'prev',
+            selected: 'selected',
+            next: 'next',
+            nextRightSecond: 'nextRightSecond',
+            hideRight: 'hideRight'
+        })
     }
 
-    moveNext2 = () => {
-        console.log('n2')
+    next1MoveToSelected = () => {
+        this.setState({
+            hideLeft: 'hideRight',
+            prevLeftSecond: 'hideLeft',
+            prev: 'prevLeftSecond',
+            selected: 'prev',
+            next: 'selected',
+            nextRightSecond: 'next',
+            hideRight: 'nextRightSecond'
+        })
     }
 
-    state = {
-        i: 2,
-        hideLeft2Style: {transition: 'transform 1s ease-out, width 1s ease-out, height 1s ease-out, left 1s ease-out, opacity 1s ease-out', left: '-30%', zIndex: '0', height: '17vw', maxHeight: '30vh', width: '10vw', maxWidth: '32vh', top: '50%', transform: 'translate(0, -50%)', opacity: '0'},
-        hideLeft2Click: null,
-        hideLeft1Style: {transition: 'transform 1s ease-out, width 1s ease-out, height 1s ease-out, left 1s ease-out, opacity 1s ease-out', left: '-15%', zIndex: '1', height: '22vw', maxHeight: '40vh', width: '15vw', maxWidth: '37vh', top: '50%', transform: 'translate(0, -50%)', opacity: '0'},
-        hideLeft1Click: null,
-        prev2Style: {transition: 'transform 1s ease-out, width 1s ease-out, height 1s ease-out, left 1s ease-out, opacity 1s ease-out', left: '0%', zIndex: '2', height: '27vw', maxHeight: '50vh', width: '20vw', maxWidth: '42vh', top: '50%', transform: 'translate(0, -50%)'},
-        prev2Click: this.movePrev2,
-        prev1Style: {transition: 'transform 1s ease-out, width 1s ease-out, height 1s ease-out, left 1s ease-out, opacity 1s ease-out', left: '15%', zIndex: '3', height: '32vw', maxHeight: '60vh', width: '25vw', maxWidth: '47vh', top: '50%', transform: 'translate(0, -50%)'},
-        prev1Click: this.movePrev1,
-        selectedStyle: {transition: 'transform 1s ease-out, width 1s ease-out, height 1s ease-out, left 1s ease-out, opacity 1s ease-out', left: '50%', transform: 'translate(-50%, -50%)', zIndex: '4', top: '50%', height: '37vw', maxHeight: '70vh', width: '30vw', maxWidth: '57vh'},
-        selectedClick: this.moveSelected,
-        next1Style: {transition: 'transform 1s ease-out, width 1s ease-out, height 1s ease-out, left 1s ease-out, opacity 1s ease-out', left: '85%', zIndex: '3', height: '32vw', maxHeight: '60vh', width: '25vw', maxWidth: '47vh', top: '50%', transform: 'translate(-100%, -50%)'},
-        next1Click: this.moveNext1,
-        next2Style: {transition: 'transform 1s ease-out, width 1s ease-out, height 1s ease-out, left 1s ease-out, opacity 1s ease-out', left: '100%', zIndex: '2', height: '27vw', maxHeight: '50vh', width: '20vw', maxWidth: '42vh', top: '50%', transform: 'translate(-100%, -50%)'},
-        next2Click: this.moveNext2,
-        hideRight1Style: {transition: 'transform 1s ease-out, width 1s ease-out, height 1s ease-out, left 1s ease-out, opacity 1s ease-out', left: '98%', zIndex: '1', height: '22vw', maxHeight: '40vh', width: '15vw', maxWidth: '37vh', top: '50%', transform: 'translate(0, -50%)', opacity: '0'},
-        hideRight1Click: null,
-        hideRight2Style: {transition: 'transform 1s ease-out, width 1s ease-out, height 1s ease-out, left 1s ease-out, opacity 1s ease-out', left: '110%', zIndex: '0', height: '17vw', maxHeight: '30vh', width: '10vw', maxWidth: '32vh', top: '50%', transform: 'translate(0, -50%)', opacity: '0'},
-        hideRight2Click: null
+    next2MoveToSelected = () => {
+        this.setState({
+            hideLeft: 'nextRightSecond',
+            prevLeftSecond: 'hideRight',
+            prev: 'hideLeft',
+            selected: 'prevLeftSecond',
+            next: 'prev',
+            nextRightSecond: 'selected',
+            hideRight: 'next'
+        })
+    }
+
+    next3MoveToSelected = () => {
+        this.setState({
+            hideLeft: 'next',
+            prevLeftSecond: 'nextRightSecond',
+            prev: 'hideRight',
+            selected: 'hideLeft',
+            next: 'prevLeftSecond',
+            nextRightSecond: 'prev',
+            hideRight: 'selected'
+        })
     }
 
     render() {
-        const users = this.props.allUsers.map(user => <DisplayUser key={user.id} user={user}/>)
-        console.log(users)
-        console.log(this.state.prev2Click)
         return(
-            <div id="UserList_carousel">
-                {/* <div id='UserList_prev2'>{(users[this.state.i - 2]) ? users[this.state.i - 2] : null}</div>
-                <div id='UserList_prev1'>{(users[this.state.i - 1]) ? users[this.state.i - 1] : null}</div>
-                <div id='UserList_selected'>{(users[this.state.i]) ? users[this.state.i] : null}</div>
-                <div id='UserList_next1'>{(users[this.state.i + 1]) ? users[this.state.i + 1] : null}</div>
-                <div id='UserList_next2'>{(users[this.state.i + 2]) ? users[this.state.i + 2] : null}</div> */}
-                <div id='1' className='UserList_img' onClick={this.state.hideLeft2Click} style={this.state.hideLeft2Style}></div>
-                <div id='2' className='UserList_img' onClick={this.state.hideLeft1Click} style={this.state.hideLeft1Style}></div>
-                <div id='3' className='UserList_img' onClick={this.state.prev2Click} style={this.state.prev2Style}></div>
-                <div id='4' className='UserList_img' onClick={this.state.prev1Click} style={this.state.prev1Style}></div>
-                <div id='5' className='UserList_img' onClick={this.state.selectedClick} style={this.state.selectedStyle}></div>
-                <div id='-4' className='UserList_img' onClick={this.state.next1Click} style={this.state.next1Style}></div>
-                <div id='-3' className='UserList_img' onClick={this.state.clickNext2} style={this.state.next2Style}></div>
-                <div id='-2' className='UserList_img' onClick={this.state.hideRight1Click} style={this.state.hideRight1Style}></div>
-                <div id='-1' className='UserList_img' onClick={this.state.hideRight2Click} style={this.state.hideRight2Style}></div>
+            <div id="carousel">
+                <div onClick={this.prev3MoveToSelected} className={this.state.hideLeft}>
+                    <img src="https://res.cloudinary.com/dzhnhtkyv/image/upload/v1538313310/Netflix42/ugo.jpg"/>
+                </div>
+
+                <div onClick={this.prev2MoveToSelected} className={this.state.prevLeftSecond}>
+                    <img src="https://res.cloudinary.com/dzhnhtkyv/image/upload/v1538313310/Netflix42/ugo.jpg"/>
+                </div>
+
+                <div onClick={this.prev1MoveToSelected} className={this.state.prev}>
+                    <img src="https://res.cloudinary.com/dzhnhtkyv/image/upload/v1538313310/Netflix42/ugo.jpg"/>
+                </div>
+
+                <div onClick={this.moveToSelected} className={this.state.selected}>
+                    <img src="https://res.cloudinary.com/dzhnhtkyv/image/upload/v1538313310/Netflix42/ugo.jpg"/>
+                </div>
+
+                <div onClick={this.next1MoveToSelected} className={this.state.next}>
+                    <img src="https://res.cloudinary.com/dzhnhtkyv/image/upload/v1538313310/Netflix42/ugo.jpg"/>
+                </div>
+
+                <div onClick={this.next2MoveToSelected} className={this.state.nextRightSecond}>
+                    <img src="https://res.cloudinary.com/dzhnhtkyv/image/upload/v1538313310/Netflix42/ugo.jpg"/>
+                </div>
+
+                <div onClick={this.next3MoveToSelected} className={this.state.hideRight}>
+                    <img src="https://res.cloudinary.com/dzhnhtkyv/image/upload/v1538313310/Netflix42/ugo.jpg"/>
+                </div>
             </div>
         )
     }
