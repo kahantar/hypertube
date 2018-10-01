52b000c6cd9ecfcb9194805ee896980bb2735f42
Ugo Nicolai <unicolai@student.42.fr>
2018-10-02 00:11:00 +0200

    Users integrated almost done

--- frontend/src/components/users/usersList.js
+++ frontend/src/components/users/usersList.js
@@ -83,15 +83,6 @@ class UsersList extends React.Component{
     //     hideRight2Style: {transition: 'transform 1s ease-out, width 1s ease-out, height 1s ease-out, left 1s ease-out, opacity 1s ease-out', left: '110%', zIndex: '0', height: '17vw', maxHeight: '30vh', width: '10vw', maxWidth: '32vh', top: '50%', transform: 'translate(0, -50%)', opacity: '0'},
     //     hideRight2Click: null
 
-    state = {
-        hideLeft: 'hideLeft',
-        prevLeftSecond: 'prevLeftSecond',
-        prev: 'prev',
-        selected: 'selected',
-        next: 'next',
-        nextRightSecond: 'nextRightSecond',
-        hideRight: 'hideRight'
-    }
 
     // moveToSelected = (e) => {
     //     console.log(e)
@@ -144,7 +135,46 @@ class UsersList extends React.Component{
     //   });
     // }
 
+    state = {
+        idPrev3: this.props.allUsers.length - 3,
+        idPrev2: this.props.allUsers.length - 2,
+        idPrev: this.props.allUsers.length - 1,
+        idSelected: 0,
+        idNext: 1,
+        idNext2: 2,
+        idNext3: 3,
+        hideLeft: 'hideLeft',
+        prevLeftSecond: 'prevLeftSecond',
+        prev: 'prev',
+        selected: 'selected',
+        next: 'next',
+        nextRightSecond: 'nextRightSecond',
+        hideRight: 'hideRight'
+    }
+
     prev3MoveToSelected = () => {
+        let newId1
+        let newId2
+        let newId3
+
+        if (this.state.hideLeft === 'prevLeftSecond') {
+            newId1 = (this.state.idNext3 - 2 === -1) ? this.props.allUsers.length - 1 : (this.state.idNext3 - 2 === -2) ? this.props.allUsers.length - 2 : this.state.idNext3 - 2
+            newId2 = this.state.idSelected
+            newId3 = (this.state.idNext3 - 1 === -1) ? this.props.allUsers.length - 1 : this.state.idNext3 - 1
+        }
+        if (this.state.hideLeft === 'prev') {
+            newId1 = (this.state.idNext2 - 1 === -1) ? this.props.allUsers.length - 1 : this.state.idNext2 - 1
+            newId2 = this.state.idSelected
+        }
+        else if (this.state.hideLeft === 'selected') {
+            newId1 = this.state.idNext
+            newId2 = this.state.idSelected
+        }
+        else if (this.state.hideLeft === 'next') {
+            newId1 = this.state.idNext
+            newId2 = (this.state.idPrev + 1 === this.props.allUsers.length) ? 0 : this.state.idPrev + 1
+        }
+
         this.setState({
             hideLeft: 'selected',
             prevLeftSecond: 'next',
@@ -152,11 +182,36 @@ class UsersList extends React.Component{
             selected: 'hideRight',
             next: 'hideLeft',
             nextRightSecond: 'prevLeftSecond',
-            hideRight: 'prev'
+            hideRight: 'prev',
+            idNext: newId1,
+            idSelected: newId2,
+            idNext2: newId3
         })
     }
 
     prev2MoveToSelected = () => {
+        let newId1
+        let newId2
+        let newId3
+
+        if (this.state.prevLeftSecond === 'prevLeftSecond') {
+            newId1 = (this.state.idPrev3 - 2 === -1) ? this.props.allUsers.length - 1 : (this.state.idPrev3 - 2 === -2) ? this.props.allUsers.length - 2 : this.state.idPrev3 - 2
+            newId2 = this.state.idNext
+            newId3 = (this.state.idPrev3 - 1 === -1) ? this.props.allUsers.length - 1 : this.state.idPrev3 - 1
+        }
+        else if (this.state.prevLeftSecond === 'prev') {
+            newId1 = (this.state.idNext3 - 1 === -1) ? this.props.allUsers.length - 1 : this.state.idNext3 - 1
+            newId2 = this.state.idNext
+        }
+        else if (this.state.prevLeftSecond === 'selected') {
+            newId1 = this.state.idNext2
+            newId2 = this.state.idNext
+        }
+        else if (this.state.prevLeftSecond === 'next') {
+            newId1 = this.state.idNext2
+            newId2 = (this.state.idSelected + 1 === this.props.allUsers.length) ? 0 : this.state.idSelected + 1
+        }
+
         this.setState({
             hideLeft: 'prev',
             prevLeftSecond: 'selected',
@@ -164,11 +219,38 @@ class UsersList extends React.Component{
             selected: 'nextRightSecond',
             next: 'hideRight',
             nextRightSecond: 'hideLeft',
-            hideRight: 'prevLeftSecond'
+            hideRight: 'prevLeftSecond',
+            idNext2: newId1,
+            idNext: newId2,
+            idNext3: newId3
         })
     }
 
     prev1MoveToSelected = () => {
+        let newId1
+        let newId2
+        let newId3
+
+        if (this.state.prev === 'prevLeftSecond') {
+            console.log('HEREE')
+
+            newId1 = (this.state.idPrev2 - 2 === -1) ? this.props.allUsers.length - 1 : (this.state.idPrev2 - 2 === -2) ? this.props.allUsers.length - 2 : this.state.idPrev2 - 2
+            newId2 = this.state.idNext2
+            newId3 = (this.state.idPrev2 - 1 === -1) ? this.props.allUsers.length - 1 : this.state.idPrev2 - 1
+        }
+        else if (this.state.prev === 'prev') {
+            newId1 = (this.state.idPrev3 - 1 === -1) ? this.props.allUsers.length - 1 : this.state.idPrev3 - 1
+            newId2 = this.state.idNext2
+        }
+        else if (this.state.prev === 'selected') {
+            newId1 = this.state.idNext3
+            newId2 = this.state.idNext2
+        }
+        else if (this.state.prev === 'next') {
+            newId1 = this.state.idNext3
+            newId2 = (this.state.idNext + 1 === this.props.allUsers.length) ? 0 : this.state.idNext + 1
+        }
+
         this.setState({
             hideLeft: 'prevLeftSecond',
             prevLeftSecond: 'prev',
@@ -176,11 +258,36 @@ class UsersList extends React.Component{
             selected: 'next',
             next: 'nextRightSecond',
             nextRightSecond: 'hideRight',
-            hideRight: 'hideLeft'
+            hideRight: 'hideLeft',
+            idNext3: newId1,
+            idNext2: newId2,
+            idPrev3: newId3
         })
     }
 
     moveToSelected = () => {
+        let newId1
+        let newId2
+        let newId3
+
+        if (this.state.selected === 'prevLeftSecond') {
+            newId1 = (this.state.idPrev - 2 === -1) ? this.props.allUsers.length - 1 : (this.state.idPrev - 2 === -2) ? this.props.allUsers.length - 2 : this.state.idPrev - 2
+            newId2 = this.state.idNext3
+            newId3 = (this.state.idPrev - 1 === -1) ? this.props.allUsers.length - 1 : this.state.idPrev - 1
+        }
+        else if (this.state.selected === 'prev') {
+            newId1 = (this.state.idPrev2 - 1 === -1) ? this.props.allUsers.length - 1 : this.state.idPrev2 - 1
+            newId2 = this.state.idNext3
+        }
+        else if (this.state.selected === 'selected') {
+            newId1 = this.state.idPrev3
+            newId2 = this.state.idNext3
+        }
+        else if (this.state.selected === 'next') {
+            newId1 = this.state.idPrev3
+            newId2 = (this.state.idNext2 + 1 === this.props.allUsers.length) ? 0 : this.state.idNext2 + 1
+        }
+
         this.setState({
             hideLeft: 'hideLeft',
             prevLeftSecond: 'prevLeftSecond',
@@ -188,11 +295,36 @@ class UsersList extends React.Component{
             selected: 'selected',
             next: 'next',
             nextRightSecond: 'nextRightSecond',
-            hideRight: 'hideRight'
+            hideRight: 'hideRight',
+            idPrev3: newId1,
+            idNext3: newId2,
+            idPrev2: newId3
         })
     }
 
     next1MoveToSelected = () => {
+        let newId1
+        let newId2
+        let newId3
+
+        if (this.state.next === 'prevLeftSecond') {
+            newId1 = (this.state.idSelected - 2 === -1) ? this.props.allUsers.length - 1 : (this.state.Selected - 2 === -2) ? this.props.allUsers.length - 2 : this.state.idSelected - 2
+            newId2 = this.state.idPrev3
+            newId3 = (this.state.idSelected - 1 === -1) ? this.props.allUsers.length - 1 : this.state.idSelected - 1
+        }
+        else if (this.state.next === 'prev') {
+            newId1 = (this.state.idPrev - 1 === -1) ? this.props.allUsers.length - 1 : this.state.idPrev - 1
+            newId2 = this.state.idNext2
+        }
+        else if (this.state.next === 'selected') {
+            newId1 = this.state.idPrev2
+            newId2 = this.state.idNext2
+        }
+        else if (this.state.next === 'next') {
+            newId1 = this.state.idPrev2
+            newId2 = (this.state.idNext3 + 1 === this.props.allUsers.length) ? 0 : this.state.idNext3 + 1
+        }
+
         this.setState({
             hideLeft: 'hideRight',
             prevLeftSecond: 'hideLeft',
@@ -200,11 +332,37 @@ class UsersList extends React.Component{
             selected: 'prev',
             next: 'selected',
             nextRightSecond: 'next',
-            hideRight: 'nextRightSecond'
+            hideRight: 'nextRightSecond',
+            idPrev2: newId1,
+            idPrev3: newId2,
+            idPrev: newId3
         })
     }
 
     next2MoveToSelected = () => {
+        let newId1
+        let newId2
+        let newId3
+
+        if (this.state.nextRightSecond === 'prevLeftSecond') {
+            newId1 = (this.state.idNext - 2 === -1) ? this.props.allUsers.length - 1 : (this.state.idNext - 2 === -2) ? this.props.allUsers.length - 2 : this.state.idNext - 2
+            newId2 = this.state.idPrev2
+            newId3 = (this.state.idNext - 1 === -1) ? this.props.allUsers.length - 1 : this.state.idNext - 1
+        }
+        else if (this.state.nextRightSecond === 'prev') {
+            console.log('here')
+            newId1 = (this.state.idSelected - 1 === -1) ? this.props.allUsers.length - 1 : this.state.idSelected - 1
+            newId2 = this.state.idPrev2
+        }
+        else if (this.state.nextRightSecond === 'selected') {
+            newId1 = this.state.idPrev
+            newId2 = this.state.idPrev2
+        }
+        else if (this.state.nextRightSecond === 'next') {
+            newId1 = this.state.idPrev
+            newId2 = (this.state.idPrev3 + 1 === this.props.allUsers.length) ? 0 : this.state.idPrev3 + 1
+        }
+
         this.setState({
             hideLeft: 'nextRightSecond',
             prevLeftSecond: 'hideRight',
@@ -212,11 +370,36 @@ class UsersList extends React.Component{
             selected: 'prevLeftSecond',
             next: 'prev',
             nextRightSecond: 'selected',
-            hideRight: 'next'
+            hideRight: 'next',
+            idPrev: newId1,
+            idPrev2: newId2,
+            idSelected: newId3
         })
     }
 
     next3MoveToSelected = () => {
+        let newId1
+        let newId2
+        let newId3
+
+        if (this.state.hideRight === 'prevLeftSecond') {
+            newId1 = (this.state.idNext2 - 2 === -1) ? this.props.allUsers.length - 1 : (this.state.Next2 - 2 === -2) ? this.props.allUsers.length - 2 : this.state.idNext2 - 2
+            newId2 = this.state.idPrev
+            newId3 = (this.state.idNext2 - 1 === -1) ? this.props.allUsers.length - 1 : this.state.idNext2 - 1
+        }
+        else if (this.state.hideRight === 'prev') {
+            newId1 = (this.state.idNext - 1 === -1) ? this.props.allUsers.length - 1 : this.state.idNext - 1
+            newId2 = this.state.idPrev
+        }
+        else if (this.state.hideRight === 'selected') {
+            newId1 = this.state.idSelected
+            newId2 = this.state.idPrev
+        }
+        else if (this.state.hideRight === 'next') {
+            newId1 = this.state.idSelected
+            newId2 = (this.state.idPrev2 + 1 === this.props.allUsers.length) ? 0 : this.state.idPrev2 + 1
+        }
+
         this.setState({
             hideLeft: 'next',
             prevLeftSecond: 'nextRightSecond',
@@ -224,45 +407,65 @@ class UsersList extends React.Component{
             selected: 'hideLeft',
             next: 'prevLeftSecond',
             nextRightSecond: 'prev',
-            hideRight: 'selected'
+            hideRight: 'selected',
+            idSelected: newId1,
+            idPrev: newId2,
+            idNext: newId3
         })
     }
 
     render() {
-        console.log(this.props.allUsers[0])
+        console.log(this.state)
+        console.log(this.props.allUsers)
         return(
             <div id="carousel">
-                <div onClick={this.prev3MoveToSelected} className='box' id={this.state.hideLeft}>
-                    <div className='img' style={{backgroundImage: `url('https://res.cloudinary.com/dzhnhtkyv/image/upload/v1538313310/Netflix42/ugo.jpg')`}}/>
-                    {/* <DisplayUser key={this.props.allUsers[0].id} user={this.props.allUsers[0]}/> */}
-
-                </div>
-
-                <div onClick={this.prev2MoveToSelected} className='box' id={this.state.prevLeftSecond}>
-                    <div className='img' style={{backgroundImage: `url('https://res.cloudinary.com/dzhnhtkyv/image/upload/v1538313310/Netflix42/ugo.jpg')`}}/>
-                </div>
-
-                <div onClick={this.prev1MoveToSelected} className='box' id={this.state.prev}>
-                    <div className='img' style={{backgroundImage: `url('https://res.cloudinary.com/dzhnhtkyv/image/upload/v1538313310/Netflix42/ugo.jpg')`}}/>
-                </div>
-
-                <div onClick={this.moveToSelected} className='box' id={this.state.selected}>
-                    <div className='img' style={{backgroundImage: `url(${this.props.allUsers[0].img})`}}>
-                        <div className='UsersList_txt' id='UsersList_username'>{this.props.allUsers[0].username}</div>
-                        <div className='UsersList_txt' id='UsersList_firstSecondName'>{this.props.allUsers[0].first_name} {this.props.allUsers[0].name}</div>
+                <div data-id={this.state.idPrev3} onClick={this.prev3MoveToSelected} className='box' id={this.state.hideLeft}>
+                    <div className='img' style={{backgroundImage: `url(${this.props.allUsers[this.state.idPrev3].img})`}}>
+                        <div className='UsersList_txt' id='UsersList_username'>{this.props.allUsers[this.state.idPrev3].username}</div>
+                        <div className='UsersList_txt' id='UsersList_firstSecondName'>{this.props.allUsers[this.state.idPrev3].first_name} {this.props.allUsers[this.state.idPrev3].name}</div>
                     </div>
                 </div>
 
-                <div onClick={this.next1MoveToSelected} className='box' id={this.state.next}>
-                    <div className='img' style={{backgroundImage: `url('https://res.cloudinary.com/dzhnhtkyv/image/upload/v1538313310/Netflix42/ugo.jpg')`}}/>
+                <div data-id={this.state.idPrev2} onClick={this.prev2MoveToSelected} className='box' id={this.state.prevLeftSecond}>
+                    <div className='img' style={{backgroundImage: `url(${this.props.allUsers[this.state.idPrev2].img})`}}>
+                        <div className='UsersList_txt' id='UsersList_username'>{this.props.allUsers[this.state.idPrev2].username}</div>
+                        <div className='UsersList_txt' id='UsersList_firstSecondName'>{this.props.allUsers[this.state.idPrev2].first_name} {this.props.allUsers[this.state.idPrev2].name}</div>
+                    </div>
                 </div>
 
-                <div onClick={this.next2MoveToSelected} className='box' id={this.state.nextRightSecond}>
-                    <div className='img' style={{backgroundImage: `url('https://res.cloudinary.com/dzhnhtkyv/image/upload/v1538313310/Netflix42/ugo.jpg')`}}/>
+                <div data-id={this.state.idPrev} onClick={this.prev1MoveToSelected} className='box' id={this.state.prev}>
+                    <div className='img' style={{backgroundImage: `url(${this.props.allUsers[this.state.idPrev].img})`}}>
+                        <div className='UsersList_txt' id='UsersList_username'>{this.props.allUsers[this.state.idPrev].username}</div>
+                        <div className='UsersList_txt' id='UsersList_firstSecondName'>{this.props.allUsers[this.state.idPrev].first_name} {this.props.allUsers[this.state.idPrev].name}</div>
+                    </div>
                 </div>
 
-                <div onClick={this.next3MoveToSelected} className='box' id={this.state.hideRight}>
-                    <div className='img' style={{backgroundImage: `url('https://res.cloudinary.com/dzhnhtkyv/image/upload/v1538313310/Netflix42/ugo.jpg')`}}/>
+                <div data-id={this.state.idSelected} onClick={this.moveToSelected} className='box' id={this.state.selected}>
+                    <div className='img' style={{backgroundImage: `url(${this.props.allUsers[this.state.idSelected].img})`}}>
+                        <div className='UsersList_txt' id='UsersList_username'>{this.props.allUsers[this.state.idSelected].username}</div>
+                        <div className='UsersList_txt' id='UsersList_firstSecondName'>{this.props.allUsers[this.state.idSelected].first_name} {this.props.allUsers[this.state.idSelected].name}</div>
+                    </div>
+                </div>
+
+                <div data-id={this.state.idNext} onClick={this.next1MoveToSelected} className='box' id={this.state.next}>
+                    <div className='img' style={{backgroundImage: `url(${this.props.allUsers[this.state.idNext].img})`}}>
+                        <div className='UsersList_txt' id='UsersList_username'>{this.props.allUsers[this.state.idNext].username}</div>
+                        <div className='UsersList_txt' id='UsersList_firstSecondName'>{this.props.allUsers[this.state.idNext].first_name} {this.props.allUsers[this.state.idNext].name}</div>
+                    </div>
+                </div>
+
+                <div data-id={this.state.idNext2} onClick={this.next2MoveToSelected} className='box' id={this.state.nextRightSecond}>
+                    <div className='img' style={{backgroundImage: `url(${this.props.allUsers[this.state.idNext2].img})`}}>
+                        <div className='UsersList_txt' id='UsersList_username'>{this.props.allUsers[this.state.idNext2].username}</div>
+                        <div className='UsersList_txt' id='UsersList_firstSecondName'>{this.props.allUsers[this.state.idNext2].first_name} {this.props.allUsers[this.state.idNext2].name}</div>
+                    </div>
+                </div>
+
+                <div data-id={this.state.idNext3} onClick={this.next3MoveToSelected} className='box' id={this.state.hideRight}>
+                    <div className='img' style={{backgroundImage: `url(${this.props.allUsers[this.state.idNext3].img})`}}>
+                        <div className='UsersList_txt' id='UsersList_username'>{this.props.allUsers[this.state.idNext3].username}</div>
+                        <div className='UsersList_txt' id='UsersList_firstSecondName'>{this.props.allUsers[this.state.idNext3].first_name} {this.props.allUsers[this.state.idNext3].name}</div>
+                    </div>
                 </div>
             </div>
         )
